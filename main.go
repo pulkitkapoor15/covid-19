4b1ae45ecf88a629608b0086dab61ebcc411656e
pulkit kapoor <pulkitkapoor15@gmail.com>
2020-06-27 03:41:10 +0530

    Remove comment

--- main.go
+++ main.go
@@ -10,20 +10,6 @@ import (
 	"github.com/rs/cors"
 )
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:8001
-// @BasePath /idea-box
 func main() {
 	route := routes.GetRoutes()
 	handler := cors.AllowAll().Handler(route)
